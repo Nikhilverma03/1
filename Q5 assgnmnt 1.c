3b1989a82a521b68db0ecab0ccd83288c9359363
Nikhilverma03 <109453250+Nikhilverma03@users.noreply.github.com>
2022-07-17 13:30:23 +0530

    Add files via upload

---
+++ Q5 assgnmnt 1.c
@@ -0,0 +1,10 @@
+//5. WAP to calculate the length of String using printf function.
+
+#include<stdio.h>
+int main()
+{
+   int L;
+   L=printf("Nikhil");
+   printf("\nLength of string is %d",L);
+   return 0;
+}
